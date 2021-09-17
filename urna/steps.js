de490932335b7869798c87719a8bd1c8e8f0175c
guilhermeabel99 <gpeuvis@gmail.com>
2021-09-17 18:39:06 -0300

    initial commit

---
+++ urna/steps.js
@@ -0,0 +1,50 @@
+let steps = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Reginaldo',
+        partido: 'PT',
+        fotos: [
+          { url: 'reginaldo.jpg', legenda: 'Vereador', small: false }
+        ]
+      },
+      {
+        numero: '77222',
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos: [
+          { url: '77222.jpg', legenda: 'Vereador' }
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Daniel',
+        partido: 'PSDB',
+        vice: 'Geovana',
+        fotos: [
+          { url: 'reginaldo.jpg', legenda: 'Prefeito' },
+          { url: 'geovana.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'Fulano',
+        partido: 'WASD',
+        vice: 'Ful',
+        fotos: [
+          { url: '84.jpg', legenda: 'Prefeito' },
+          { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+    ]
+  }
+];
